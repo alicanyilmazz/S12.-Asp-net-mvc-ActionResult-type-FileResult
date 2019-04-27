bce10d2576629e823b7d91320621abf6de886774
alicanyilmazz <alicanyilmazz@users.noreply.github.com>
2019-04-27 21:44:27 +0300

    Add project files.

---
+++ FileResult_sample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileResult_sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult homepage()
+        {
+            return View();
+        }
+
+        public FileResult Show_File()
+        {
+            string file_path = Server.MapPath("~/Files/java.pdf");
+
+            return new FilePathResult(file_path, "application/pdf");
+        }
+    }
+}
---
+++ FileResult_sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FileResult_sample.MvcApplication" Language="C#" %>
